f33aca2057bd7a5410d3ec620d922ab03fe6e0a8
Yi Li <yl622@njit.edu>
2017-10-01 19:29:35 -0400

    finished question 3 and skipped question 2

--- index.php
+++ index.php
@@ -9,21 +9,40 @@ $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
 print_r($year);
 
-echo '<hr />Replace “-“ in $date with “/“ and print out the result.: </br>';
-print (str_replace ('-','/',$date) . '</br>');
+echo '<hr />Replace “-“ in $date with “/“ and print out the result.: </br></br>';
+$date = str_replace ('-','/',$date);
+print ($date . '</br>');
 
 echo '<hr />Compare $date with $tar and then if the result is greater than 0, you should print out “the
 future”; if the result is less than 0, you should print out “the past”; if the result is equal to 0,
-you should print out “Oops”. You must use if-elseif statement in this question.</br>';
-if ($date > $tar) {
-  echo 'the future</br>';
-} elseif ($date < $tar) {
-  echo 'the past</br>';
+you should print out “Oops”. You must use if-elseif statement in this question: </br></br>';
+//if (($date <=> $tar) > 0 ) {
+//  echo 'the future</br>';
+//} elseif (($date < $tar) < 0) {
+//  echo 'the past</br>';
+//} else {
+//  echo 'Oops</br>';
+//}
+
+echo '<hr />Search for “/“ in $date and print out all positions. If there are more than one position, please
+delimit each position value with space: </br></br>';
+$pos = strpos ($date, '/');
+if ($pos  === false ) {
+  echo '"/" was not found in "$date"</br>';
 } else {
-  echo 'Oops</br>';
-  print ($date-$tar);
+  print ($pos);
+  $i = 1;
+  while ($i > 0) {
+    $pos2 = strpos ($date, '/', $pos + 1);
+    if ($pos2  === false) {
+      echo '</br>';
+      break;
+    } else {
+      print (' ' . $pos2);
+      $pos = $pos2;
+    }
+  }
 }
-  
 
 
 ?>
