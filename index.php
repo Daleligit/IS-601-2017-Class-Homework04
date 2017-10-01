0a58d4a48ff32f014a037df4899f791addc48f20
Yi Li <yl622@njit.edu>
2017-10-01 19:02:59 -0400

    finished question 1 and tried to do question 2

--- index.php
+++ index.php
@@ -7,6 +7,23 @@ echo "The value of \$tar: ".$tar."<br>";
 
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
-print_r($year)
+print_r($year);
+
+echo '<hr />Replace “-“ in $date with “/“ and print out the result.: </br>';
+print (str_replace ('-','/',$date) . '</br>');
+
+echo '<hr />Compare $date with $tar and then if the result is greater than 0, you should print out “the
+future”; if the result is less than 0, you should print out “the past”; if the result is equal to 0,
+you should print out “Oops”. You must use if-elseif statement in this question.</br>';
+if ($date > $tar) {
+  echo 'the future</br>';
+} elseif ($date < $tar) {
+  echo 'the past</br>';
+} else {
+  echo 'Oops</br>';
+  print ($date-$tar);
+}
+  
+
 
 ?>
